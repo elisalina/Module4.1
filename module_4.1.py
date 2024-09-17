8736a5cceed932aca05e18726f19b51f34eae01d
Алина Демчук <alina2805@bk.ru>
2024-09-17 22:10:40 +0300

    Add files via upload

---
+++ module_4.1.py
@@ -0,0 +1,7 @@
+import fake_math as fm
+import true_math as tm
+def fake_divide():
+    result1 = fake_divide(69, 3)
+    result2 = fake_divide(3, 0)
+    result3 = true_divide(49, 7)
+    result4 = true_divide(15, 0)
---
+++ true_math.py
@@ -0,0 +1,11 @@
+from math import inf
+
+def true_divide(first, second):
+    if second == 0:
+        return inf
+    return first / second
+
+result3 = true_divide(49, 7)
+result4 = true_divide(15, 0)
+print(result3)
+print(result4)
